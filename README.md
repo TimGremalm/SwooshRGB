895f4869d9378d48db64f8606792f06d09ea2afc
Tim Gremalm <tim@gremalm.se>
2021-09-23 23:31:54 +0200

    Listen to sACN and PWM RGBW accordingly

---
+++ README.md
@@ -0,0 +1,2 @@
+# SwooshRGB
+PWM RGB led strip over sACN (E1.31, DMX512) esp-idf
---
+++ main/dmxlight.h
@@ -0,0 +1,8 @@
+#ifndef DMXLIGHT_H_
+#define DMXLIGHT_H_
+
+#include <stdint.h>
+
+void dmxlighttask(void *pvParameters);
+
+#endif /* DMXLIGHT_H_ */
